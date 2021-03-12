6551d0e516bfa436b32ecd96525ab1bd514f0597
FujimuraKaito <>
2021-03-12 09:13:31 +0900

    :memo: make dynamic routing

--- next_tutorial/nextjs-blog/pages/posts/[id].js
+++ next_tutorial/nextjs-blog/pages/posts/[id].js
@@ -1,16 +1,36 @@
 import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 // Reactコンポーネント
-export default function Post() {
-  return <Layout>...</Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      { postData.title }
+      <br />
+      { postData.id }
+      <br />
+      { postData.date }
+    </Layout>
+  )
 }
 
 // 必要
 export async function getStaticPaths() {
   // idとして取りうる値のリストを返す
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 // 必要
 export async function getStaticProps({ params }) {
   // params.idを使用してブログ投稿に必要なデータを取得する
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
