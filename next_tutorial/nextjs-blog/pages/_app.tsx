d841292d8bf72a0e0626a75c1914ab8842d68fc3
FujimuraKaito <>
2021-03-12 15:05:41 +0900

    :package: introduce tailwind css

--- next_tutorial/nextjs-blog/pages/_app.tsx
+++ next_tutorial/nextjs-blog/pages/_app.tsx
@@ -1,9 +1,16 @@
+// global.cssはimportするだけで良い？
 import '../styles/global.css'
 import { AppProps } from 'next/app'
 // import { Component } from 'react'
 
-// 全てのページに共通するトップレベルのコンポーネント
-// このファイルを追加時にはサーバーを再起動
+/**
+ * 全てのページに共通するトップレベルのコンポーネント
+ * このファイルを追加時にはサーバーを再起動
+ * ページ間の共通レイアウトを持たせることができる
+ * 共通のstateを持つことができる
+ * グローバルなCSSを定義できる
+ */
+
 
 // JS version
 // function App({ Component, pageProps }: AppProps) {
